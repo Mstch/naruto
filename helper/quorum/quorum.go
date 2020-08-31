b90be2fa181c12dc23ca97b4909e2a735226d4fb
tiddar <1124206990@qq.com>
2020-08-31 20:46:25 +0800

    feat: add quorum helper

--- helper/quorum/quorum.go
+++ helper/quorum/quorum.go
@@ -1,1 +1,30 @@
 package quorum
+
+import "sync/atomic"
+
+type q struct {
+	cur            uint32
+	quorum         uint32
+	quorumCallback func()
+}
+
+var (
+	quorumMap        = make(map[uint64]*q)
+	lastId    uint64 = 0
+)
+
+func RegQuorum(quorum uint32, quorumCallback func()) uint64 {
+	id := atomic.AddUint64(&lastId, 1)
+	quorumMap[id] = &q{
+		cur:            0,
+		quorum:         quorum,
+		quorumCallback: quorumCallback,
+	}
+	return id
+}
+func Approve(id uint64) {
+	q := quorumMap[id]
+	if atomic.AddUint32(&q.cur, 1) > atomic.LoadUint32(&q.quorum) {
+		delete(quorumMap,id)
+	}
+}
